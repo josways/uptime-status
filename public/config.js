a6b3aaefba1382b2d94f3dd52a2e3f618bbc5afa
josway <jxh98@foxmail.com>
2023-03-29 22:14:11 +0800

    Use my own key

--- public/config.js
+++ public/config.js
@@ -6,10 +6,9 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    // 'm791819989-b640e70b6a8ebe264d22def0',
+    // 'm791810356-167ce7ac7e5649fcb244931a',
+    'ur1737176-dc025b00b9f11987c08becbc',
   ],
 
   // 日志天数
@@ -22,15 +21,15 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'https://www.josway.cc/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/josways/uptime-status'
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://blog.josway.cc/'
     },
   ],
 };
